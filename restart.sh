08a9cc9fe67b292cb6f69a13e71211b095ef9431
Christin Löhner <github@r3y.de>
2022-02-24 18:50:25 +0100

    formatting

--- restart.sh
+++ restart.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
- 
+
 DOCKERPATH=/server/jitsi.partei-mut.de
- 
+
 # Shutdown jitsi meet containers and remove images
 cd $DOCKERPATH
 docker-compose down --rmi all
- 
+
 # Delete configs and recreate folders
-sudo rm -rf .jitsi-meet-cfg/
+sudo rm -rf .jitsi-meet-cfg
 mkdir -p .jitsi-meet-cfg/{web/letsencrypt,transcripts,prosody,jicofo,jvb}
- 
+
 # Start Jitsi Meet
 docker-compose up -d
 sleep 10
- 
+
 # Customize
 ./customize.sh
- 
+
 # Set JVB Logging from INFO to WARNING
 sudo sed -i 's/^.level=.*/.level=WARNING/' .jitsi-meet-cfg/jvb/logging.properties
 docker restart jitsipartei-mutde_jvb_1
